41a5394566bcf18cbc019c8cfcbf983af0f61745
Constantin Mussaeus <cmussaeus@student.ethz.ch>
2021-12-03 13:12:31 +0100

    improved day3

--- src/day3.rs
+++ src/day3.rs
@@ -49,62 +49,68 @@ pub fn solve_day3_part1(input: &Vec<Line>) -> u64 {
 
 #[aoc(day3, part2)]
 pub fn solve_day3_part2(input: &Vec<Line>) -> u64 {
-    getBigger(&mut input.clone()) * getSmaller(&mut input.clone())
+    let mut gamma: Vec<bool> = vec![];
+    let mut epsilon: Vec<bool> = vec![];
+    for y in 0..input[0].len() {
+        let mut count0_gamma: u64 = 0;
+        let mut count0_epsilon: u64 = 0;
+        let mut count1_gamma: u64 = 0;
+        let mut count1_epsilon: u64 = 0;
+        let mut nums_epsilon: u64 = 0;
+        for x in 0..input.len() {
+            if check(&gamma, &input[x]) {
+                if input[x][y] {
+                    count1_gamma += 1;
+                } else {
+                    count0_gamma += 1;
+                }
+            }
+            if check(&epsilon, &input[x]) {
+                nums_epsilon += 1;
+                if input[x][y] {
+                    count1_epsilon += 1;
+                } else {
+                    count0_epsilon += 1;
+                }
+            }
+        }
+        if count1_gamma >= count0_gamma {
+            gamma.push(true);
+        } else {
+            gamma.push(false);
+        }
+        if nums_epsilon < 2 {
+            if count0_epsilon > count1_epsilon {
+                epsilon.push(false);
+            } else {
+                epsilon.push(true);
+            }
+        } else if count1_epsilon < count0_epsilon {
+            epsilon.push(true);
+        } else {
+            epsilon.push(false);
+        }
+    }
+    let mut a: u64 = 0;
+    let mut b: u64 = 0;
+    for (i, x) in gamma.iter().rev().enumerate() {
+        if *x {
+            a += (2 as u64).pow(i as u32);
+        }
+    }
+    for (i, x) in epsilon.iter().rev().enumerate() {
+        if *x {
+            b += (2 as u64).pow(i as u32);
+        }
+    }
+    a * b
 }
 
-fn getBigger(rows: &mut Vec<Vec<bool>>) -> u64 {
-    for col in 0..rows[0].len() {
-        let mut count0: u64 = 0;
-        let mut count1: u64 = 0;
-        for row in 0..rows.len() {
-            if rows[row][col] {
-                count1 += 1;
-            } else {
-                count0 += 1;
-            }
-        }
-        if count1 >= count0 {
-            rows.retain(|x| x[col]);
-        } else {
-            rows.retain(|x| !x[col]);
-        }
-        if rows.len() < 2 {
-            break;
+fn check(a: &Vec<bool>, b: &Vec<bool>) -> bool {
+    for (x, y) in a.iter().zip(b.iter()) {
+        if x != y {
+            return false;
         }
     }
-    let mut res: u64 = 0;
-    for (i, x) in rows[0].iter().rev().enumerate() {
-        if *x {
-            res += (2 as u64).pow(i as u32);
-        }
-    }
-    res
-}
-fn getSmaller(rows: &mut Vec<Vec<bool>>) -> u64 {
-    for col in 0..rows[0].len() {
-        let mut count0: u64 = 0;
-        let mut count1: u64 = 0;
-        for row in 0..rows.len() {
-            if rows[row][col] {
-                count1 += 1;
-            } else {
-                count0 += 1;
-            }
-        }
-        if count1 < count0 {
-            rows.retain(|x| x[col]);
-        } else {
-            rows.retain(|x| !x[col]);
-        }
-        if rows.len() < 2 {
-            break;
-        }
-    }
-    let mut res: u64 = 0;
-    for (i, x) in rows[0].iter().rev().enumerate() {
-        if *x {
-            res += (2 as u64).pow(i as u32);
-        }
-    }
-    res
+    return true;
 }
