cdf118983f5f12873bafa11f4198f88208f00a6b
Constantin Mussaeus <cmussaeus@student.ethz.ch>
2021-12-12 08:02:46 +0100

    using HashSets for faster contains

--- src/day12.rs
+++ src/day12.rs
@@ -32,16 +32,21 @@ pub fn generator_day12(input: &str) -> Inp {
 #[aoc(day12, part1)]
 pub fn solve_day12_part1(input: &Inp) -> u64 {
     let mut count: u64 = 0;
-    dfs("start".to_string(), input, &vec![], &mut count);
+    dfs(
+        "start".to_string(),
+        input,
+        &HashSet::with_capacity(10),
+        &mut count,
+    );
     count
 }
 
-fn dfs(current: String, list_of_nodes: &Inp, visited_old: &Vec<String>, count: &mut u64) {
+fn dfs(current: String, list_of_nodes: &Inp, visited_old: &HashSet<String>, count: &mut u64) {
     let mut visited = visited_old.clone();
-    visited.push(current.clone());
+    visited.insert(current.clone());
     if &current == "end" {
         *count += 1;
-        visited.pop();
+        visited.remove("end");
         return;
     }
     for next in list_of_nodes.get(&current).unwrap() {
@@ -55,28 +60,34 @@ fn dfs(current: String, list_of_nodes: &Inp, visited_old: &Vec<String>, count: &
         }
         dfs(next.clone(), list_of_nodes, &visited, count);
     }
-    visited.pop();
+    visited.remove(&current);
 }
 
 #[aoc(day12, part2)]
 pub fn solve_day12_part2(input: &Inp) -> u64 {
     let mut count: u64 = 0;
-    dfs2("start".to_string(), input, &vec![], &mut count, false);
+    dfs2(
+        "start".to_string(),
+        input,
+        &HashSet::with_capacity(10),
+        &mut count,
+        false,
+    );
     count
 }
 
 fn dfs2(
     current: String,
     list_of_nodes: &Inp,
-    visited_old: &Vec<String>,
+    visited_old: &HashSet<String>,
     count: &mut u64,
     twice: bool,
 ) {
     let mut visited = visited_old.clone();
-    visited.push(current.clone());
+    visited.insert(current.clone());
     if &current == "end" {
         *count += 1;
-        visited.pop();
+        visited.remove("end");
         return;
     }
     for next in list_of_nodes.get(&current).unwrap() {
@@ -93,5 +104,5 @@ fn dfs2(
         }
         dfs2(next.clone(), list_of_nodes, &visited, count, twice);
     }
-    visited.pop();
+    visited.remove(&current);
 }
